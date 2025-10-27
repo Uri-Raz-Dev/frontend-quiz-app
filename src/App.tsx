fdba93ed173123a84afe597e95c776be4c97471c
Uri_Raz <uri1515@gmail.com>
2025-10-27 21:49:55 +0200

    Create mobile design for Home page

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,23 @@ import Home from './pages/Home'
 import Quiz from './pages/Quiz'
 import Score from './pages/Score'
 import { Switch } from './components/ui/switch'
-
+import sun_icon_dark from './assets/images/icon-sun-dark.svg'
+import moon_icon_dark from './assets/images/icon-moon-dark.svg'
 function App() {
   return (
     <BrowserRouter>
-      <Switch />
+      <div className="flex justify-between px-6 py-4">
+        <div></div>
+        <div className="flex items-center gap-2">
+          <img className="size-4" src={sun_icon_dark} />
+
+          <Switch />
+          <img className="size-4" src={moon_icon_dark} />
+        </div>
+      </div>
       <Routes>
         <Route path="/frontend-quiz-app/" element={<Home />} />
-        <Route path="/frontend-quiz-app/quiz" element={<Quiz />} />
+        <Route path="/frontend-quiz-app/quiz/*" element={<Quiz />} />
         <Route path="/frontend-quiz-app/score" element={<Score />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,45 @@
+import { Link } from 'react-router-dom'
+import data from '../../data.json'
+import html_icon from '../assets/images/icon-html.svg'
+import css_icon from '../assets/images/icon-css.svg'
+import js_icon from '../assets/images/icon-js.svg'
+import accessibility_icon from '../assets/images/icon-accessibility.svg'
+
 const Home = () => {
-  return <main className="min-h-[100dvh]"></main>
+  const createList = () => {
+    const quizList = data.quizzes
+    const iconArray = [html_icon, css_icon, js_icon, accessibility_icon]
+    const iconColorArray = [
+      'bg-(--color-orange50)',
+      'bg-(--color-green100)',
+      'bg-(--color-blue50)',
+      'bg-(--color-purple100)',
+    ]
+    const quizArray = quizList.map((val, idx) => {
+      return (
+        <li className="option flex items-center gap-4 p-4" key={val.title}>
+          <div
+            className={`image-container flex items-center justify-center ${iconColorArray[idx]}`}
+          >
+            <img src={iconArray[idx]} />
+          </div>
+          <Link to={`/frontend-quiz-app/quiz/${val.title}`}>{val.title}</Link>
+        </li>
+      )
+    })
+    return quizArray
+  }
+  return (
+    <main className="flex flex-col gap-10 px-6 pt-8">
+      <section className="flex flex-col gap-4">
+        <h1 className="text-pretty">
+          Welcome to the <span>Frontend Quiz!</span>
+        </h1>
+        <h2>Pick a subject to get started</h2>
+      </section>
+      <ul className="flex flex-col gap-4">{createList()}</ul>
+    </main>
+  )
 }
 
 export default Home
