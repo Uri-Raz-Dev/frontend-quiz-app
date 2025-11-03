a830b443c2c61e31ab080a36bc45462625934f87
Uri_Raz <uri1515@gmail.com>
2025-11-04 00:21:53 +0200

    Fix a mistake of margin at Header comp

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
   const { isDark } = useQuiz()
 
   return (
-    <header className="lg: mt-13.5 flex justify-between px-6 py-4 md:px-0 md:py-0 lg:mt-0 lg:mb-25">
+    <header className="flex justify-between px-6 py-4 md:mt-13.5 md:px-0 md:py-0 lg:my-24.25 lg:mb-25">
       {id ? <Logo /> : <div></div>}
       <div className="flex items-center gap-2">
         <img
